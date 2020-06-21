24469dda1e5a883b589782fffb32cf1dc5f69d57
Andres Botello <andres.botello@estudios-electricos.com>
2020-06-21 18:38:59 -0300

    agregado de codigo de ejemplo en C# .NET

---
+++ AsyncAndMultiThread/AsyncAndMultiThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndMultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            escribeUnaLetraX();
+            escribeUnaLetraY();
+            Console.ReadKey();*/
+            
+            
+            Thread t = new Thread(escribeUnaLetraY);
+            t.Start();
+            escribeUnaLetraX();
+            Console.ReadKey();
+        }
+        static void escribeUnaLetraX()
+        {
+            escribeUnaLetra('X');
+        }
+        static void escribeUnaLetraY()
+        {
+            escribeUnaLetra('Y');
+        }
+        static void escribeUnaLetra(Char letra)
+        {
+            /*for (int i = 0; i < 1000; i++) Console.Write(letra);*/
+            
+            Parallel.For(0, 1000, i =>
+            {
+                Console.Write(letra);
+                i++;
+            });
+        }
+    }
+}
---
+++ AsyncAndMultiThread/ElementosDesayuno/ElementosDesayuno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementosDesayuno
+{
+    public class Coffee
+    {
+    }
+    public class Egg
+    {
+    }
+    public class Bacon
+    {
+    }
+    public class Toast
+    {
+    }
+    public class Juice
+    {
+    }
+}
